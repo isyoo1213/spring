8e7197dadd2c02d52b90a863efe1ecec5d4a8884
isyoo1213 <moethns@gmail.com>
2022-09-09 23:11:18 +0900

    feat: Add domain and repository

---
+++ user-service/src/main/java/user/userservice/domain/Member.java
@@ -0,0 +1,24 @@
+package user.userservice.domain;
+
+public class Member {
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private long id;
+    private String name;
+
+}
---
+++ user-service/src/main/java/user/userservice/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package user.userservice.repository;
+
+import user.userservice.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+    //동시성 문제로 공유되는 변수일 때는 quenqurer 뭐시기 써야하나 예시이므로 단순하게 hashMap
+    private Long sequence = 0L;
+    //이 변수도 동시성 문제를 고려해서 어텀 뭐시기 해야함
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
