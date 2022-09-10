a17a684ff6f795da8ea4fdb087dbc96620f06fbf
isyoo1213 <moethns@gmail.com>
2022-09-10 23:18:43 +0900

    feat: Add setting for Test-Class

---
+++ user-service/src/test/java/user/userservice/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,13 @@
+package user.userservice.repository;
+
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+
+    }
+}
