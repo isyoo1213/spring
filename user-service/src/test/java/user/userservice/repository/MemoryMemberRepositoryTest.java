e1f04c914b68c32f0f4c05efc7c0999c8421fe2c
isyoo1213 <moethns@gmail.com>
2022-09-12 22:59:48 +0900

    feat: Add Test-related import for testing code

--- user-service/src/test/java/user/userservice/repository/MemoryMemberRepositoryTest.java
+++ user-service/src/test/java/user/userservice/repository/MemoryMemberRepositoryTest.java
@@ -1,7 +1,10 @@
 package user.userservice.repository;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import user.userservice.domain.Member;
+
+import static org.assertj.core.api.Assertions.*;
 
 class MemoryMemberRepositoryTest {
 
@@ -13,8 +16,22 @@ class MemoryMemberRepositoryTest {
         member.setName("spring");
 
         repository.save(member);
+        //setId를 통해 id 설정 > store.put을 통해 map구조로 저장됨
 
         Member result = repository.findById(member.getId()).get();
+        //Optional에서 값을 꺼낼 때 바로 처리하는 것은 바람직하지 않으나 테스트코드이므로 간략화
         System.out.println("result == " + (result == member));
+        //member변수와 save를 통해 store에 저장어 findById를 통해 가져온 객체가 같은지 확인
+
+        //org.junit.jupiter.api 에서 제공하는 클래스
+//        assertEquals(member, result);
+        //parameter 정보보기 단축키 - ctrl + P
+        //( expected , actual )
+
+        //org.assertj.core.api 에서 제공하는 클래스 - 조금 더 직관적으로 쓸 수 있음
+        //Assersions에 Alt+Enter >> static으로 지정 후 바로 사용가능
+        assertThat(member).isEqualTo(result);
+
+
     }
 }
