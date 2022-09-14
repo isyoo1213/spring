a4016642750202cdf15d54e986dccd517a7db336
isyoo1213 <moethns@gmail.com>
2022-09-14 14:44:57 +0900

    feat: Add Test for Service

---
+++ user-service/src/test/java/user/userservice/service/MemberServiceTest.java
@@ -0,0 +1,65 @@
+package user.userservice.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import user.userservice.domain.Member;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+//Test 클래스 생성 시 default로 junit의 Assersions를 가져옴
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("insung");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOneMember(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("Spring");
+
+        Member member2 = new Member();
+        member2.setName("Spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () ->
+                memberService.join(member2));
+        //assertThrows() Parameter - 기대하는 exception, 실행되는 로직 by lambda(when)
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 이름입니다.");
+
+/*
+        try{
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e){
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 이름입니다.");
+        }
+*/
+
+        //then
+    }
+
+    @Test
+    void findAllMember() {
+    }
+
+    @Test
+    void findOneMember() {
+    }
+}
