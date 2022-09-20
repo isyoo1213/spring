6bf8a43b91ef4f2a70c1d7dc65890c6bb515d73b
isyoo1213 <moethns@gmail.com>
2022-09-20 13:52:30 +0900

    feat: Add zip file and basic settings for OOP

---
+++ oop/src/test/java/spring/oop/OopApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.oop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
