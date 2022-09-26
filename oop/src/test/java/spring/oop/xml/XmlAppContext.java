c424f78e9427ae76cbf2d75751137657d7dedd21
isyoo1213 <moethns@gmail.com>
2022-09-26 19:10:29 +0900

    feat: Add Test for Xml-config

---
+++ oop/src/test/java/spring/oop/xml/XmlAppContext.java
@@ -0,0 +1,20 @@
+package spring.oop.xml;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import spring.oop.member.MemberService;
+
+import static org.assertj.core.api.Assertions.*;
+
+class XmlAppContext {
+
+    @Test
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+
+}
