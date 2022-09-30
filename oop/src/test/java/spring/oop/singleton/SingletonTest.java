e01a26d99c32d830d95d64f56c5642cac86f4af6
isyoo1213 <moethns@gmail.com>
2022-09-30 22:30:42 +0900

    feat: Add Test for SingletonService

--- oop/src/test/java/spring/oop/singleton/SingletonTest.java
+++ oop/src/test/java/spring/oop/singleton/SingletonTest.java
@@ -31,4 +31,20 @@ class SingletonTest {
 
     }
 
+    @Test
+    @DisplayName("싱글톤 패턴을 사용한 객체 사용")
+    void singletonServiceTest(){
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+        System.out.println("인스턴스 참조값 동일성 확인 결과 = " + String.valueOf(singletonService1==singletonService2));
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        //sameAs - == (실제 instance 비교)
+        //equalTo - equals 메서드 (오버라이딩 가능)
+
+    }
+
 }
