09cd977ebb61a0771ee40607dbf81a586cc654e2
isyoo1213 <moethns@gmail.com>
2022-10-05 23:28:52 +0900

    feat: Add StatefulService

---
+++ oop/src/test/java/spring/oop/singleton/StatefulService.java
@@ -0,0 +1,12 @@
+package spring.oop.singleton;
+
+class StatefulService {
+
+    private int price; //상태를 유지하는 필드
+
+    public void order(String name, int price){
+        System.out.println("name = " + name + "\nprice = " + price);
+        this.price = price;
+    }
+
+}
