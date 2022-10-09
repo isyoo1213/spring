45b56fd49fd2f19aba2cf6738f4cedfb8fc965b3
isyoo1213 <moethns@gmail.com>
2022-10-09 23:56:27 +0900

    feat: Add Comments

--- oop/src/test/java/spring/oop/singleton/StatefulService.java
+++ oop/src/test/java/spring/oop/singleton/StatefulService.java
@@ -4,11 +4,18 @@ class StatefulService {
 
 //    private int price; //상태를 유지하는 필드
 
+
 //    public void order(String name, int price){
 //        System.out.println("name = " + name + "\nprice = " + price);
 //        this.price = price;
 //    }
 
+    /**
+     * '반환값'을 통해 order() 메서드의 price parameter 대한 의존성을 제거 --> non-stateful
+     * @param name
+     * @param price
+     * @return
+     */
     public int order(String name, int price){
         System.out.println("name = " + name + "\nprice = " + price);
         return price;
