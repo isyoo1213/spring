9d0e8d1949da93a3afac6e55d939b449fc731649
isyoo1213 <moethns@gmail.com>
2022-10-15 19:01:02 +0900

    feat: Add Comments for @ComponentScan's area

--- oop/src/main/java/spring/oop/AutoAppConfig.java
+++ oop/src/main/java/spring/oop/AutoAppConfig.java
@@ -8,6 +8,9 @@ import org.springframework.context.annotation.FilterType;
 //Configuration 어노테이션 또한 @Component 어노테이션을 포함하고 있음
 @Configuration
 @ComponentScan(
+        //탐색할 패키지 위치를 정하지 않을 경우 - @ConponentScan 어노테이션이 붙은 클래스가 위치한 패키지와 하위를 모두 탐색
+        //즉, spring.oop 하위의 모든 패키지를 탐색 --> ** 설정정보 클래스를 최상위 패키지에 위치하고 따로 설정하지 않는 방식이 관례
+        basePackages = "spring.oop.member", //원하는 패키지에서 컴포넌트스캔 가능 -> AutoAppConfig 자신을 포함한 위치 내의 component만 스캔
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
--- oop/src/main/java/spring/oop/OopApplication.java
+++ oop/src/main/java/spring/oop/OopApplication.java
@@ -3,6 +3,9 @@ package spring.oop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//해당 어노테이션도 @ComponentScan을 기본적으로 포함 --> 프로젝트 최상단에 위치하는 것이 올바름
+//즉, spring.oop 에서 시작해 하위 모든 패키지의 @Component를 스캔
+//SpringBoot를 사용하면 실질적으로 @ComponentScan 어노테이션을 사용할 필요가 없음
 @SpringBootApplication
 public class OopApplication {
 
