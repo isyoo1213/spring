936455c8e7a9ad9995345b559522278014bcf6ba
isyoo1213 <moethns@gmail.com>
2022-11-05 23:33:54 +0900

    feat: Add Zip and Settings for OOP Advanced

---
+++ oopadvanced/src/main/java/spring/oopadvanced/OopadvancedApplication.java
@@ -0,0 +1,13 @@
+package spring.oopadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopadvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopadvancedApplication.class, args);
+	}
+
+}
---
+++ oopadvanced/src/test/java/spring/oopadvanced/OopadvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.oopadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopadvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
