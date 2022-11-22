8e57b24931836ea3c72ee7f40f6b46a192fcb661
isyoo1213 <moethns@gmail.com>
2022-11-22 22:51:47 +0900

    feat: Add ItemDetail Controller Method and HTML

--- item-service/src/main/java/spring/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/spring/itemservice/web/basic/BasicItemController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import spring.itemservice.domain.item.Item;
 import spring.itemservice.domain.item.ItemRepository;
@@ -29,6 +30,13 @@ public class BasicItemController {
         List<Item> items =  itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model){
+        Item findItem = itemRepository.findById(itemId);
+        model.addAttribute("item", findItem);
+        return "basic/item";
     }
 
     /**
