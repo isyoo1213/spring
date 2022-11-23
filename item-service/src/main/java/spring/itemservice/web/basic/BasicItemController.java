5b7b3249f66a8ced6a74ac16e22eae6ada595ed0
isyoo1213 <moethns@gmail.com>
2022-11-23 22:00:55 +0900

    feat: Add AddItemV1 Html and Controller Method

--- item-service/src/main/java/spring/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/spring/itemservice/web/basic/BasicItemController.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import spring.itemservice.domain.item.Item;
 import spring.itemservice.domain.item.ItemRepository;
 
@@ -39,11 +37,38 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    //등록 Form과 등록 처리(save)를 같은 URL로 처리할 예정
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+    //위의 상품등록 단순 Form html의 뷰 네임을 반환하는 GET 메서드와 URL주소는 같고, 요청 메서드만 다르게 구성
+    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam Integer price,
+                       @RequestParam Integer quantity,
+                       Model model){
+        //Item item = new Item(itemName, price, quantity);
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        //Item 조회에서 model에 추가한 "item"과 key값이 겹침
+        //-> 조회시 model에 담기는 "item"과 상품등록시 model에 담기는 "item"은 어떻게 구별하는것일까?
+        //-> request 단위로 model 인스턴스가 생성되는 것 vs Singleton으로 관리되는 것 vs Singleton이지만 request단위로 전달되는것
+
+        return "basic/item";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
     @PostConstruct
-    public void init(){
+    public void init() {
         itemRepository.save(new Item("ItemA", 10000, 10));
         itemRepository.save(new Item("ItemB", 20000, 20));
     }
