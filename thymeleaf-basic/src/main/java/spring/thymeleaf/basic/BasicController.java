2d10f7de8be9b9e9d2c72df9cfaff789336bef15
isyoo1213 <moethns@gmail.com>
2022-11-26 07:23:05 +0900

    feat: Add HTML for Variables in Thymeleaf

--- thymeleaf-basic/src/main/java/spring/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/spring/thymeleaf/basic/BasicController.java
@@ -1,9 +1,15 @@
 package spring.thymeleaf.basic;
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -21,5 +27,37 @@ public class BasicController {
         return "basic/text-unescaped";
     }
 
+    @GetMapping("variable")
+    public String variable(Model model){
+        User userA = new User("userA", 20);
+        User userB = new User("userB", 30);
+
+        //List와 Map 자료형 처리를 확인하기 위한 객체
+        List<User> userList = new ArrayList<>();
+        userList.add(userA);
+        userList.add(userB);
+
+        Map<String, User> userMap = new HashMap<>();
+        userMap.put("userA", userA);
+        userMap.put("userB", userB);
+
+        //Model에는 User 객체, List, Map을 저장
+        model.addAttribute("user", userA);
+        model.addAttribute("userList", userList);
+        model.addAttribute("userMap", userMap);
+
+        return "basic/variable";
+    }
+
+    @Data
+    static class User{
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+    }
 
 }
