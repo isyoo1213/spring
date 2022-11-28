9ceb155770c7e0d39c1751f2b28cc65b1eab143e
isyoo1213 <moethns@gmail.com>
2022-11-28 12:48:25 +0900

    feat: Add Messages and Internationalization

--- message/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ message/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -9,5 +9,23 @@ public class ItemServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
-
+	/*
+	//스프링이 제공하는 MessageSource 인터페이스
+	//SpringBoot는 MessageSource를 자동으로 Bean등록한다 + application.properties에서 세부 설정 가능
+	//ex) spring.messages.basename=messages,config.i18n.messages
+	// 기본경로 - Resources -> 아래의 config / i18n / messages.properties를 파일로 사용한다는 의미
+	// * 별도의 Bean등록이나 스프링부트의 설정을 해주지 않는 경우의 Default 설정
+	// -> spring.messages.basename=messages
+	// -> 파일명_국가.properties 로 설정 정보 추가해주면 자동으로 국제화 설정 인식
+	//ex) messages_en.properties
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new
+				ResourceBundleMessageSource();
+		messageSource.setBasenames("messages", "errors");
+		//각각의 설정파일의 이름 설정 - 설정파일.properties 파일을 읽어들임
+		messageSource.setDefaultEncoding("utf-8");
+		return messageSource;
+	}
+*/
 }
---
+++ message/src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -0,0 +1,61 @@
+package hello.itemservice.message;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+//SpringBoot로 설정해 자동으로 message관련 설정을 불러서 등록할 수 있도록 하기 위함
+class MessageSourceTest {
+
+    @Autowired//필드 주입 - SpringBoot가 제공하는 DI를 제공받으므로 가능? 원래 고립된 순수한 단위테스트에서는 불가능
+    MessageSource messageSource;
+
+    @Test
+    void helloMessage(){
+        //오류 발생시 - Settings - File Encodings - Properties files - UTF-8인코딩 설정
+        String result = messageSource.getMessage("hello", null, null);
+        //3번째 argument인 locale 정보가 없을 경우, basename에 설정한 기본 이름의 message 설정 파일을 조회 - messages.properties
+        //2번째 인자는 arguments - .properties 파일의 {} 처리된 부분을 사용하는 인자
+        assertThat(result).isEqualTo("안녕");
+    }
+
+    @Test //설정파일에 설정된 값이 없을 경우 -> 예외발생
+    void notFoundMessageCode(){
+        assertThatThrownBy(()->messageSource.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+
+    @Test // 값이 없을 경우 '기본 메시지'를 통해 예외 피하기
+    void notFoundMessageCodeDefaultMessagge(){
+        //defaultMessage 인자 추가
+        String result = messageSource.getMessage("no_code", null, "기본 메시지", null);
+        assertThat(result).isEqualTo("기본 메시지");
+    }
+
+    @Test
+    void argumentMessage(){
+        //2번째 인자 - 설정 파일의 {}부분에 치환해서 들어감 -> Object[] 배열 타입
+        String result = messageSource.getMessage("hello.name", new Object[]{"spring"}, null);
+        assertThat(result).isEqualTo("안녕 spring");
+    }
+
+    @Test
+    void defaultLang(){
+        assertThat(messageSource.getMessage("hello", null, null)).isEqualTo("안녕");
+        assertThat(messageSource.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
+    }
+
+    @Test
+    void enLang(){
+        assertThat(messageSource.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
+    }
+
+}
