8490c9a776d64c41e6f42665c3fdb4da7d8c6d0f
isyoo1213 <moethns@gmail.com>
2022-12-02 19:53:15 +0900

    feat: Add Bean Validation on EditForm

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -96,7 +96,21 @@ public class ValidationItemControllerV3 {
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+
+        //특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() != null && item.getQuantity() != null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+
+        if(bindingResult.hasErrors()){
+            log.info("errors = {}", bindingResult);
+            return "validation/v3/editForm";
+        }
+
         itemRepository.update(itemId, item);
         return "redirect:/validation/v3/items/{itemId}";
     }
