50c11debd99acb19e906caf582c2059d09b1434e
isyoo1213 <moethns@gmail.com>
2022-12-07 17:13:34 +0900

    feat: Add Cookie for Login and Logout

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,48 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+    //@GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    //Login처리까지 되는 새로운 Home Controller
+    @GetMapping("/")
+    public String homeLogin(
+            @CookieValue(name = "memberId", required = false) Long memberId, //스프링이 Type Converting해줌
+            Model model
+    ){
+    //Cookie값 꺼내 쓰는 방법은 다양 - HttpServletRequest에서 꺼내서 사용하는 방법도 가능
+    //required = false를 통해 쿠키를 가지지 않은 비로그인 사용자도 접근 가능하도록 허용
+
+        if(memberId == null){
+            return "home";
+        }
+
+        //쿠키 존재시 로그인한 사용자 정보 확인
+        Member loginMember = memberRepository.findById(memberId);
+        if(loginMember == null){//사용자가 존재하지 않을 수도, 쿠키가 만료됐을 수도 있는 상황
+            return "home";
+        }
+
+        //View에서 로그인한 사용자 정보를 활용하기 위해서 Model에 저장
+        model.addAttribute("member", loginMember);
+
+        return "loginHome";
     }
 }
--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -19,13 +22,17 @@ public class LoginController {
     private final LoginService loginService;
 
     @GetMapping("/login")
-    public String loginForm(@ModelAttribute("loginForm") LoginForm form){
+    public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
         return "login/loginForm";
     }
 
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
+    public String login(
+            @Validated
+            @ModelAttribute("loginForm") LoginForm form,
+            BindingResult bindingResult,
+            HttpServletResponse response) {
+        if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
 
@@ -34,13 +41,35 @@ public class LoginController {
         //id, password의 오류 등 특정 필드의 문제가 아님 - MemberRepository에서 DB까지 접근해서 데이터를 확인해봐야 판단 가능
         // *** 생각해볼 것 - Login에서는 Service로 구성한 이유 - MemberRepository를 사용해 DB까지 접근하므로?
         // -> Login 자체의 도메인을 활용할 Repository를 구성하는 것보다 기존의 다른 Domain을 활용하므로 Service 계층으로 확장한 개념?
-        if(loginMember == null){
+        if (loginMember == null) {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
             return "login/loginForm";
         }
 
-        //로그인 성공 처리 TODO
+        //로그인 성공 처리 TODO - Cookie를 만들어서 저장
+
+        //Cookie 생성
+        // + *** Cookie에 시간 정보를 주지 않으면 세션 쿠키(Session과 상관 X)가 됨 -> 브라우저 종료시 종료
+        // cf) 영속쿠키
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId()));
+
+        //생성한 Cookie를 Response에 실어 보내는 작업 - HttpServletResponse 사용
+        response.addCookie(idCookie);
 
         return "redirect:/";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response){
+        //Age가 0인 Cookie를 같은 이름으로 바꿔 만료시키는 방식
+        expireCookie(response, "memberId");
+
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null); // Ctrl + Alt + P로 파라미터 변수화
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
