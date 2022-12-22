11d25157a8ab4a06994d444f18ecb11a99ba53d1
isyoo1213 <moethns@gmail.com>
2022-12-22 21:03:56 +0900

    feat: Add Getting Error-Info from WAS

--- exception/src/main/java/spring/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/spring/exception/servlet/ErrorPageController.java
@@ -10,17 +10,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ErrorPageController {
 
+    //*** Error에 관한 정보를 WAS에서 인식한 뒤, 다시 ErrorPage에 따른 Controller를 호출할 때, 단순히 호출뿐만 아니라
+    // + 오류 정보를 request의 attribute에 추가해서 넘겨준다
+    //RequestDispatcher 상수로 정의되어 있음
+    public static final String ERROR_EXCEPTION = "javax.servlet.error.exception";
+    public static final String ERROR_EXCEPTION_TYPE = "javax.servlet.error.exception_type";
+    public static final String ERROR_MESSAGE = "javax.servlet.error.message";
+    public static final String ERROR_REQUEST_URI = "javax.servlet.error.request_uri";
+    public static final String ERROR_SERVLET_NAME = "javax.servlet.error.servlet_name";
+    public static final String ERROR_STATUS_CODE = "javax.servlet.error.status_code";
+
     //HttpMethod 한번에 처리하려고 @RequestMapping 사용
     @RequestMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response){
         log.info("errorPage 404");
+        printErrorInfo(request);
         return "error-page/404"; //View 반환
     }
 
     @RequestMapping("/error-page/500")
     public String errorPage500(HttpServletRequest request, HttpServletResponse response){
         log.info("errorPage 500");
+        printErrorInfo(request);
         return "error-page/500";
+    }
+
+    private void printErrorInfo(HttpServletRequest request){
+        //오류에 대한 WAS의 오류정보 처리를 확인하는 로그
+        // ColumnSelection (Ctrl+Ctrl)로 작성연습
+        log.info("ERROR_EXCEPTION: {}", request.getAttribute(ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
+        log.info("ERROR_MESSAGE: {}", request.getAttribute(ERROR_MESSAGE));
+        log.info("ERROR_REQUEST_URI: {}", request.getAttribute(ERROR_REQUEST_URI));
+        log.info("ERROR_SERVLET_NAME: {}", request.getAttribute(ERROR_SERVLET_NAME));
+        log.info("ERROR_STATUS_CODE: {}", request.getAttribute(ERROR_STATUS_CODE));
+
+        log.info("dispatchType = {}", request.getDispatcherType());
+
+        // *** 현재 코드 실행 시 printErrorInfo() 정보 제대로 출력되지 않는 부분 추후 확인하기
     }
 
 }
