6729998dfbac39ab71877a9b5960ea6c376116d9
isyoo1213 <moethns@gmail.com>
2022-12-31 21:37:17 +0900

    feat: Add Zip and Setting for practicing TypeConverter

---
+++ typeconverter/src/test/java/spring/typeconverter/TypeconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.typeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeconverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
