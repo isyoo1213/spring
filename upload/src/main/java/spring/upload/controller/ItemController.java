0568fb5dc19804f4830af24c8f098ff196a839b2
isyoo1213 <moethns@gmail.com>
2023-01-15 22:04:58 +0900

    feat: Add Downloading Attatch-file

--- upload/src/main/java/spring/upload/controller/ItemController.java
+++ upload/src/main/java/spring/upload/controller/ItemController.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriUtils;
 import spring.upload.domain.Item;
 import spring.upload.domain.ItemRepository;
 import spring.upload.domain.UploadFile;
@@ -16,6 +19,7 @@ import spring.upload.file.FileStore;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Slf4j
@@ -65,7 +69,28 @@ public class ItemController {
     @ResponseBody
     @GetMapping("/images/{filename}")
     public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
-       return new UrlResource("file: " + fileStore.getFullPath(filename));
+        return new UrlResource("file: " + fileStore.getFullPath(filename));
     }
 
+    @GetMapping("/attach/{itemId}")
+    public ResponseEntity<Resource> downloadAttach(@PathVariable Long itemId) throws MalformedURLException {
+        //Header에 추가요소를 위해 @ResponseBody 대신 ResponseEntity 사용
+
+        //Item에 접근할 수 있는, itemId를 획득할 수 있는 사용자만 다운로드 가능할 수 있도록 구성
+        Item item = itemRepository.findById(itemId);
+        String storeFileName = item.getAttachFile().getStoreFileName();
+        String uploadFileName = item.getAttachFile().getUploadFileName();
+
+        UrlResource urlResource = new UrlResource("file : " + fileStore.getFullPath(storeFileName));
+
+        log.info("uploadFileName = {}", uploadFileName);
+
+        //다운로드를 위한 규약 - header 설정하지 않고 body만 설정 시, 다운로드가 아닌 파일의 내용이 그대로 출력됨
+        String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
+        String contentDisposition = "attachment; filename=\"" + encodedUploadFileName + "\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(urlResource);
+    }
 }
